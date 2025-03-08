cf4a7f12141aad058024deba0e302e2f4b9a7a74
lancecaintic <melbertlance.caintic@gmail.com>
2025-03-08 18:04:38 +0800

    lance commit
    # Conflicts:
    #	src/main/java/com/rocs/medical/records/application/InfirmarySystemApplication.java

---
+++ src/main/java/com/rocs/medical/records/application/data/dao/deleteMedicalRecord/StudentMedicalRecordsDao.java
@@ -0,0 +1,8 @@
+package com.rocs.medical.records.application.data.dao.deleteMedicalRecord;
+
+public interface StudentMedicalRecordsDao {
+
+    boolean deleteStudentMedicalRecordById (String Id);
+}
+
+
