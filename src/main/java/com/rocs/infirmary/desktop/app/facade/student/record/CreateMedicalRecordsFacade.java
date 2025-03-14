63f0f59400f8ef5a1859e4e33a1c80273972c226
Noaflores <noahelijahflores07@gmail.com>
2025-03-14 21:47:07 +0800

    IA-5 Create Student Medical Records

--- src/main/java/com/rocs/infirmary/desktop/app/facade/student/record/CreateMedicalRecordsFacade.java
+++
@@ -1,7 +0,0 @@
-package com.rocs.infirmary.desktop.app.facade.student.record;
-
-import com.rocs.infirmary.desktop.data.model.person.student.StudentMedicalRecords;
-
-public interface CreateMedicalRecordsFacade {
-    boolean AddStudentMedicalRecord(StudentMedicalRecords record);
-}
--- src/main/java/com/rocs/infirmary/desktop/data/dao/student/record/CreateMedicalRecordsDao.java
+++
@@ -1,9 +0,0 @@
-package com.rocs.infirmary.desktop.data.dao.student.record;
-
-import com.rocs.infirmary.desktop.data.model.person.student.StudentMedicalRecords;
-
-public interface CreateMedicalRecordsDao {
-    boolean createMedicalRecord(StudentMedicalRecords studentMedicalRecords);
-
-
-}
