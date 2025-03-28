d87b3ad3ba6d6483fecfd00caf0814c9fceb4895
KYPanlilio <kim.yashai.legaspi.panlilio@gmail.com>
2025-03-29 00:00:10 +0800

    IA - Adding New field for Visibility of Student Records and Test Data on Database (#42)
    * updated db script

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -84,6 +84,9 @@ create table medical_record (
   temperature_readings varchar2(10),
   visit_date timestamp(6),
   treatment varchar2(255),
+  status NUMBER(1,0)
+    constraint STATUS_NOT_NULL not null
+    constraint STATUS_CHECK check (status IN (0,1)),
   primary key (id));
 
 create table inventory (
@@ -196,15 +199,25 @@ alter table login
 insert into person (first_name, middle_name, last_name, birthdate, age, gender, email, address, contact_number)
 values ('John', null, 'Doe', to_timestamp('1982-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '18', 'MALE', 'johndoe@gmail.com', 'Silang, Cavite', '09173167078');
 insert into person (first_name, middle_name, last_name, birthdate, age, gender, email, address, contact_number)
-values ('Maynard', 'Kent', 'Harlan', to_timestamp('2000-01-01 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '19', 'MALE', 'harlan.maynard@gmail.com', 'Kentucky, USA', null);
+values ('Maynard', 'Kent', 'Harlan', to_timestamp('2000-01-01 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '19', 'MALE', 'harlan.maynard@gmail.com', 'Salaban', null);
 insert into person (first_name, middle_name, last_name, birthdate, age, gender, email, address, contact_number)
-values ('Joan', 'Agapito', 'dela Cruz', to_timestamp('1995-02-04 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '17', 'FEMALE', 'joan.delacruz@gmail.com', 'Makati City', '09151132244');
+values ('Joan', 'Agapito', 'dela Cruz', to_timestamp('2001-02-04 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '17', 'FEMALE', 'joan.delacruz@gmail.com', 'Trece', '09151132244');
 insert into person (first_name, middle_name, last_name, birthdate, age, gender, email, address, contact_number)
-values ('Angelo', 'Mallari', 'dela Cruz', to_timestamp('1997-04-12 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '18', 'MALE', 'angelo.delacruz@gmail.com', 'Makati City', '09151132245');
+values ('Angelo', 'Mallari', 'dela Cruz', to_timestamp('2000-04-12 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '18', 'MALE', 'angelo.delacruz@gmail.com', 'Amadeo, Cavite', '09151132245');
 insert into person (first_name, middle_name, last_name, birthdate, age, gender, email, address, contact_number)
 values ('Joshua', 'Martinez', 'Villanueva', to_timestamp('2005-12-21 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '20', 'MALE', 'joshua.villanueva@gmail.com', 'Imus, Cavite', '09175532245');
 insert into person (first_name, middle_name, last_name, birthdate, age, gender, email, address, contact_number)
-values ('Maria', 'Antonio', 'Agustin', to_timestamp('2015-09-17 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '19', 'FEMALE', 'maria.agustin@gmail.com', 'Calamba, Laguna', '09175532333');
+values ('Maria', 'Antonio', 'Agustin', to_timestamp('2003-09-17 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '19', 'FEMALE', 'maria.agustin@gmail.com', 'Dasma', '09175532333');
+insert into person (first_name, middle_name, last_name, birthdate, age, gender, email, address, contact_number)
+values ('RC', null, 'Bayot', to_timestamp('2003-10-17 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '21', 'FEMALE', 'rcbayot@gmail.com', 'Salaban', '09482156321');
+insert into person (first_name, middle_name, last_name, birthdate, age, gender, email, address, contact_number)
+values ('Alice', 'Marie', 'Johnson', TO_TIMESTAMP('1995-05-10 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '28', 'FEMALE', 'alice.johnson@gmail.com', 'Mahogany', '09178234567');
+insert into person (first_name, middle_name, last_name, birthdate, age, gender, email, address, contact_number)
+values ('Leo', 'Antonio', 'Santos', TO_TIMESTAMP('1998-08-22 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '27', 'MALE', 'leo.santos@gmail.com', 'Silang, Cavite', '09215467890');
+insert into person (first_name, middle_name, last_name, birthdate, age, gender, email, address, contact_number)
+values ('Emily', null, 'Smith', TO_TIMESTAMP('1989-03-15 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '34', 'FEMALE', 'emily.smith@gmail.com', 'Silang, Cavite', '09326789012');
+insert into person (first_name, middle_name, last_name, birthdate, age, gender, email, address, contact_number)
+values ('Mark', 'David', 'Brown', TO_TIMESTAMP('2002-07-04 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), '23', 'MALE', 'mark.brown@gmail.com', 'Salaban', '09437890123');
 
 --INSERT LOGIN DATA
 insert into login (username, password, person_id, join_date, last_login_date, role, authorities, is_active, is_locked)
@@ -229,6 +242,16 @@ insert into guardian_details (guardian_name, guardian_address, guardian_contact_
 values ('lei marinduque', 'Trece', '09524781639');
 insert into guardian_details (guardian_name, guardian_address, guardian_contact_number)
 values ('don ambion', 'Amadeo', null);
+insert into guardian_details (guardian_name, guardian_address, guardian_contact_number)
+values ('Linda Cruz', 'Bagong Silang, Caloocan', '09351234876');
+insert into guardian_details (guardian_name, guardian_address, guardian_contact_number)
+values ('Marco Reyes', 'Poblacion, Batangas', '09471234567');
+insert into guardian_details (guardian_name, guardian_address, guardian_contact_number)
+values ('Susan Lim', 'Fort Bonifacio, Taguig', null);
+insert into guardian_details (guardian_name, guardian_address, guardian_contact_number)
+values ('Greg Santos', 'Balagtas, Bulacan', '09283455678');
+insert into guardian_details (guardian_name, guardian_address, guardian_contact_number)
+values ('Anna Flores', 'New Manila, Quezon City', null);
 
 -- INSERT SECTION DATA
 insert into section (adviser_id, strand, grade_level, section)
@@ -253,6 +276,10 @@ insert into student(person_id, section_section_id, stud_guardian_id, LRN)
 values ('4', '04', '004', '012846539215');
 insert into student(person_id, section_section_id, stud_guardian_id, LRN)
 values ('5', '05', '005', '082456138297');
+insert into student(person_id, section_section_id, stud_guardian_id, LRN)
+values ('6', '01', '005', '082461532145');
+insert into student(person_id, section_section_id, stud_guardian_id, LRN)
+values ('7', '03', '004', '012846539215');
 
 -- INSERT EMPLOYEE DATA
 insert into employee (id, employee_id)
@@ -277,6 +304,16 @@ insert into ailments (ailment_id, description)
 values (4, 'Dry Cough');
 insert into ailments (ailment_id, description)
 values (5, 'Stomachache');
+insert into ailments (ailment_id, description)
+values (6, 'Skin Rash');
+insert into ailments (ailment_id, description)
+values (7, 'Shortness of Breath');
+insert into ailments (ailment_id, description)
+values (8, 'Dizziness');
+insert into ailments (ailment_id, description)
+values (9, 'Sore Throat');
+insert into ailments (ailment_id, description)
+values (10, 'Allergic Reaction');
 
 --INSERT MEDICINE DATA
 insert into medicine (medicine_id, item_name, description, expiration_date)
@@ -289,6 +326,16 @@ insert into medicine (medicine_id, item_name, description, expiration_date)
 values ('AC', 'Antacid', 'a medicine used to treat heartburn and indigestion', to_timestamp('2028-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
 insert into medicine (medicine_id, item_name, description, expiration_date)
 values ('AH', 'Antihistamine', 'treat allergic rhinitis, common cold, influenza, and other allergies', to_timestamp('2026-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
+insert into medicine (medicine_id, item_name, description, expiration_date)
+values ('AS', 'Aspirin', 'Used to treat pain, fever, or inflammation', TO_TIMESTAMP('2027-06-15 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
+insert into medicine (medicine_id, item_name, description, expiration_date)
+values ('VM', 'Vitamin C', 'Boosts immunity and promotes overall health', TO_TIMESTAMP('2026-10-10 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
+insert into medicine (medicine_id, item_name, description, expiration_date)
+values ('HD', 'Hydrocortisone Cream', 'Treats skin irritation, eczema, and rashes', TO_TIMESTAMP('2025-12-31 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
+insert into medicine (medicine_id, item_name, description, expiration_date)
+values ('DN', 'Decongestant', 'Relieves nasal congestion caused by colds', TO_TIMESTAMP('2026-03-20 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
+insert into medicine (medicine_id, item_name, description, expiration_date)
+values ('AN', 'Antibiotics', 'Treats bacterial infections in the body', TO_TIMESTAMP('2027-08-25 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
 
  --INSERT INVENTORY DATA
 insert into inventory (medicine_id, item_type, quantity)
@@ -301,16 +348,40 @@ insert into inventory (medicine_id, item_type, quantity)
 values ('AC', 'Medicine', 25);
 insert into inventory (medicine_id, item_type, quantity)
 values ('AH', 'Medicine', 20);
+insert into inventory (medicine_id, item_type, quantity)
+values ('AS', 'Medicine', 60);
+insert into inventory (medicine_id, item_type, quantity)
+values ('VM', 'Medicine', 10);
+insert into inventory (medicine_id, item_type, quantity)
+values ('HD', 'Medicine', 15);
+insert into inventory (medicine_id, item_type, quantity)
+values ('DN', 'Medicine', 50);
+insert into inventory (medicine_id, item_type, quantity)
+values ('AN', 'Medicine', 4);
 
 --INSERT MEDICAL RECORD DATA
-insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment)
-values (1, 1, null, 1, 'Severe headache', '37.5°C', to_timestamp('2000-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Prescribed pain reliever');
-insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment)
-values (2, 2, null, 2, 'Runny nose and cough', '37.2°C', to_timestamp('2008-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Rest and hydration');
-insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment)
-values (3, 3, null, 3, 'High fever', '38.5°C', to_timestamp('2025-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Prescribed antipyretic');
-insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment)
-values (4, 4, null, 4, 'Dry cough', '37.0°C', to_timestamp('2024-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Prescribed cough syrup');
+insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment, status)
+values (1, 1, null, 1, 'Headache', '37.5°C', to_timestamp('2000-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Prescribed pain reliever', 1);
+insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment, status)
+values (2, 2, null, 2, 'Stomachache', '37.2°C', to_timestamp('2008-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Rest and hydration', 0);
+insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment, status)
+values (3, 3, null, 3, 'Fever', '38.5°C', to_timestamp('2025-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Prescribed antipyretic', 1);
+insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment, status)
+values (4, 4, null, 4, 'Dry cough', '37.0°C', to_timestamp('2024-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Prescribed cough syrup', 0);
+insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment, status)
+values (5, 1, null, 3, 'Dizziness', '37.0°C', to_timestamp('2020-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Recommended hydration', 1);
+insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment, status)
+values (3, 1, null, 4, 'Headache', '37.0°C', to_timestamp('2023-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Prescribed pain reliver', 0);
+insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment, status)
+values (6, 6, null, 2, 'Skin rash', '36.9°C', TO_TIMESTAMP('2022-04-20 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Prescribed antihistamine cream', 1);
+insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment, status)
+values (1, 9, null, 3, 'Sore throat', '37.4°C', TO_TIMESTAMP('2021-03-15 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Recommended salt water gargle', 1);
+INSERT INTO medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment, status)
+values (1, 8, null, 1, 'Dizziness', '36.8°C', TO_TIMESTAMP('2020-08-14 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Recommended hydration', 0);
+insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment, status)
+values (4, 10, null, 4, 'Allergic reaction', '36.7°C', TO_TIMESTAMP('2023-09-30 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Prescribed antihistamine tablets', 1);
+insert into medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment, status)
+values (7, 7, null, 2, 'Shortness of breath', '37.0°C', TO_TIMESTAMP('2024-06-05 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'), 'Prescribed inhaler', 0);
 
 --INSERT MEDICINE ADMINISTERED
 insert into medicine_administered (id, medicine_id, med_record_id, nurse_in_charge_id, description, quantity, date_administered)
@@ -321,5 +392,17 @@ insert into medicine_administered (id, medicine_id, med_record_id, nurse_in_char
 values (3, 'PT', 3, '1', 'Paracetamol 500mg administered', 1, to_timestamp('2025-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
 insert into medicine_administered (id, medicine_id, med_record_id, nurse_in_charge_id, description, quantity, date_administered)
 values (4, 'AC', 4, '3', 'Antacid 500mg administered', 2, to_timestamp('2024-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
+insert into medicine_administered (id, medicine_id, med_record_id, nurse_in_charge_id, description, quantity, date_administered)
+values (5, 'VM', 5, '1', 'Vitamin C 500mg administered', 1, to_timestamp('2020-01-02 00:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
+insert into medicine_administered (id, medicine_id, med_record_id, nurse_in_charge_id, description, quantity, date_administered)
+values (6, 'IB', 6, '2', 'Ibuprofen 200mg administered', 1, TO_TIMESTAMP('2023-02-15 09:30:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
+insert into medicine_administered (id, medicine_id, med_record_id, nurse_in_charge_id, description, quantity, date_administered)
+values (7, 'AH', 7, '1', 'Antihistamine cream administered', 2, TO_TIMESTAMP('2022-07-10 15:45:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
+insert into medicine_administered (id, medicine_id, med_record_id, nurse_in_charge_id, description, quantity, date_administered)
+values (8, 'IB', 8, '4', 'Ibuprofen 400mg administered', 1, TO_TIMESTAMP('2021-05-25 12:20:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
+insert into medicine_administered (id, medicine_id, med_record_id, nurse_in_charge_id, description, quantity, date_administered)
+values (9, 'DN', 9, '3', 'Decongestant 10ml administered', 1, TO_TIMESTAMP('2020-10-01 08:15:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
+insert into medicine_administered (id, medicine_id, med_record_id, nurse_in_charge_id, description, quantity, date_administered)
+values (10, 'AN', 10, '2', 'Antibiotics 500mg administered', 1, TO_TIMESTAMP('2023-03-20 11:00:00.00', 'yyyy-mm-dd hh24:mi:ss:ff'));
 
 commit;
