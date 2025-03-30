1db10a72252ce6d7369e19f7f4d3a26483a6101a
Arsssie <maria.arssiely.delmundo@gmail.com>
2025-03-30 23:28:49 +0800

    Ia 35 unit test reports on search student information (#43)
    * test
    
    * get added
    
    * unit test added
    
    * student facade unit test added
    
    * changes
    
    * changes
    
    * e
    
    * changes
    
    * get added
    
    * unit test added
    
    * student facade unit test added
    
    * changess
    
    * student deleted
    
    * update 1
    
    * update 2
    
    * Search Student info test has been added
    
    * commit changes
    
    * changes

--- src/test/java/com/rocs/infirmary/desktop/data/dao/report/dashboard/impl/DashboardDaoImplTest.java
+++ src/test/java/com/rocs/infirmary/desktop/data/dao/report/dashboard/impl/DashboardDaoImplTest.java
@@ -35,7 +35,7 @@ public class DashboardDaoImplTest {
 
     @Mock
     private ResultSet resultSet;
-    
+
     private static MockedStatic<ConnectionHelper> connectionHelper;
 
     @BeforeEach
@@ -77,6 +77,7 @@ public class DashboardDaoImplTest {
         verify(preparedStatement, times(1)).setTimestamp(eq(3), any(Timestamp.class));
         verify(preparedStatement, times(1)).executeQuery();
 
+
     }
 
     @Test
@@ -98,6 +99,8 @@ public class DashboardDaoImplTest {
         verify(preparedStatement, times(1)).setTimestamp(eq(1), any(Timestamp.class));
         verify(preparedStatement, times(1)).setTimestamp(eq(2), any(Timestamp.class));
         verify(preparedStatement, times(1)).executeQuery();
+
+
 
     }
 
