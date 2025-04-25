6d4239bf06e2a9210e6b1a592fa64d410ef334c7
NNeonael <neonael.angcaya.nazareno@gmail.com>
2025-04-26 01:43:30 +0800

    AI-29 Fix Input validation for student medical record by LRN (#67)
    * fixed query-contant
    
    * Initial Commit
    
    * Initial Commit
    
    * Fixed Changes
    
    * Fixed Changes
    
    * IA-18 Delete Medicine
    
    * Updated Filtering Available Medicine
    
    * Updated Changes
    
    * AI-29 Fix Input Validation Student Medical Record By LRN

--- src/main/java/com/rocs/infirmary/desktop/InfirmarySystemApplication.java
+++ src/main/java/com/rocs/infirmary/desktop/InfirmarySystemApplication.java
@@ -137,16 +137,15 @@ public class InfirmarySystemApplication {
                     scanner.nextLine();
                     StudentMedicalRecordFacadeImpl studentMedicalRecord = new StudentMedicalRecordFacadeImpl();
                     System.out.println("Search Student Medical Records using LRN: ");
-                    long LRN = scanner.nextLong();
-                    String lrnString = String.valueOf(LRN);
+                    String LRN = scanner.next();
 
-                    if(!lrnString.matches("\\d{11}")){
+                    if(LRN.length() != 12  ){
                         LOGGER.info("User entered invalid LRN length" );
                         System.out.println("Error: Please enter a valid 12-Digit LRN.");
                         break;
                     }
 
-                    Student record = studentMedicalRecord.findMedicalInformationByLRN(LRN);
+                    Student record = studentMedicalRecord.findMedicalInformationByLRN(Long.parseLong(LRN));
                     if (record == null ) {
 
                         LOGGER.info("No student record found ");
